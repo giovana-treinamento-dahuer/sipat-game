3bffcc8c19089a206adff7d642297fa22e2fb3d1
giovana-treinamento-dahuer <giovana.treinamento@dahuer.com.br>
2024-05-08 10:40:23 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -88,8 +88,8 @@ function createPipe() {
     newPipe.classList.add('pipeTransition');
 
     const screenHeight = window.innerHeight;
-    const pipeWidth = screenHeight * 0.15;
-    const pipeHeight = screenHeight * 0.20;
+    const pipeWidth = screenHeight * 0.1;
+    const pipeHeight = screenHeight * 0.2;
 
     if (pipeSprite === '../images/pipe.png') {
         newPipe.style.width = `${pipeWidth}px`;
