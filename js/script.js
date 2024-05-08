a25deeeb1229ab9f2897aabfd3bd636c0b08e6ac
giovana-treinamento-dahuer <giovana.treinamento@dahuer.com.br>
2024-05-08 10:47:19 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -89,7 +89,7 @@ function createPipe() {
 
     const screenHeight = window.innerHeight;
     const pipeWidth = screenHeight * 0.05;
-    const pipeHeight = screenHeight * 0.01;
+    const pipeHeight = screenHeight * 0.03;
 
     if (pipeSprite === '../images/pipe.png') {
         newPipe.style.width = `${pipeWidth}px`;
