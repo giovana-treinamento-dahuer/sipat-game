09fd59bc9468a9922be8619c1db63b1e1085d307
giovana-treinamento-dahuer <giovana.treinamento@dahuer.com.br>
2024-05-09 17:53:52 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -95,7 +95,7 @@ function createPipe() {
         newPipe.style.height = '17vw';
     }
 
-    newPipe.style.left = '150vw';
+    newPipe.style.left = '300vw';
     document.querySelector('.game-board').appendChild(newPipe);
     pipes.push(newPipe);
     movePipe(newPipe);
