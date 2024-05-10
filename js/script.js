f3540df51e5f1cb35545e5837588d3e1377e237b
giovana-treinamento-dahuer <giovana.treinamento@dahuer.com.br>
2024-05-10 09:53:26 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -89,7 +89,7 @@ function createPipe() {
     newPipe.classList.add('pipeTransition');
 
     if (pipeSprite === '../images/pipe.png') {
-        newPipe.style.width = '12vw';
+        newPipe.style.width = '18vw';
     } else if (pipeSprite === '../images/box1.png') {
         newPipe.style.width = '12vw';
         newPipe.style.height = '12vw';
